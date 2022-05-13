5d406aefab3f4fb4ac3fa898a2ae3958a23355b1
Shinji <shinji@shinji.rocks>
2022-05-14 06:56:15 +0800

    updaets

--- init_from_text.py
+++ init_from_text.py
@@ -7,6 +7,7 @@ for file in sys.argv[1:]:
     with open(file) as f:
         all_text = f.read().split('\n')
         for text in all_text:
+            text = text.strip()
             if not text:
                 continue
             output = f'{output}\n{cnt},<|endoftext|>{text}<|endoftext|>'
--- install_requirements2.sh
+++ install_requirements2.sh
@@ -16,10 +16,11 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda=11.3.0-1
+sudo apt-get -y install cmake
 
 
 
-conda create -n gptj python=3.9 -y
+conda create -n gptj python=3.7 -y
 conda activate gptj
 conda install conda-forge::mamba -y
 mamba install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
@@ -34,9 +35,8 @@ pip install jax jaxlib
 pip install triton
 
 #need to drastically lower requirements
-export DS_BUILD_SPARSE_ATTN=0
 export DS_BUILD_OPS=1
-git clone https://github.com/microsoft/DeepSpeed -b v0.6.4
+git clone https://github.com/microsoft/DeepSpeed -b v0.5.8
 cd DeepSpeed
 pip install .
 cd ..
