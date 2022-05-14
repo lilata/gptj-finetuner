faa86e4edd28df337c5e25df8ec72afbbbe5a0f9
Shinji <shinji@shinji.rocks>
2022-05-14 09:01:24 +0800

    update converter

--- init_from_text.py
+++ init_from_text.py
@@ -5,13 +5,9 @@ output = ',text'
 cnt = 0
 for file in sys.argv[1:]:
     with open(file) as f:
-        all_text = f.read().split('\n')
-        for text in all_text:
-            text = text.strip()
-            if not text:
-                continue
-            output = f'{output}\n{cnt},<|endoftext|>{text}<|endoftext|>'
-            cnt += 1
+        text = f.read().strip().replace('\n', ' ').replace('\t', '').replace('"', '""')
+        output = f'{output}\n{cnt},"<|endoftext|>{text}<|endoftext|>"'
+        cnt += 1
 
 with open('train.csv', 'w') as f:
     f.write(output)
