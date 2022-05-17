90d062cd5e77a9b309546bbd906972cd8daa537e
Shinji <shinji@shinji.rocks>
2022-05-17 19:15:19 +0800

    a

--- train_data/init_from_text.py
+++ train_data/init_from_text.py
@@ -9,7 +9,7 @@ for file in sys.argv[1:]:
         text = f.read().strip().replace('\n', ' ').replace('\t', '').replace('"', '""')
         splitted = text.split(' ')
         for n in range(math.ceil(len(splitted) / 800)):
-            output = f'{output}\n{cnt},"<|endoftext|>{" ".join(splitted[n: 800*(n+1)])}<|endoftext|>"'
+            output = f'{output}\n{cnt},"<|endoftext|>{" ".join(splitted[n*800: 800*(n+1)])}<|endoftext|>"'
             cnt += 1
 
 with open('train.csv', 'w') as f:
